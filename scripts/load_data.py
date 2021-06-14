baba24154ee7e7e6de145ce6d5a72c6e44930bcb
Basia <jurzystab@gmail.com>
2021-06-14 22:15:12 +0200

    update of dataloader to the new competition

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,28 +1,39 @@
 import pandas as pd
-import pylab
-import matplotlib as plt
 import numpy as np
+import os
+from PIL import Image
+import pydicom
 import cv2
-import torch
 from torch.utils.data import Dataset, DataLoader
-import preprocessing
+from os import listdir, walk
+import torch
+from skimage import exposure
 
 
-labels = pd.read_csv('/content/drive/MyDrive/ADP/train_labels.csv', dtype={'image_id':'str',	'InChI':'str'})
-labels = labels[labels['image_id'].str.startswith('0')]
-labels['InChI'] = [s[9:] for s in labels['InChI']]     # removing 'InChI=1S/'
-labels = labels.sample(frac=1)
-n = len(labels)
-val_n = int(n/10)
-labels_val = labels.iloc[:val_n]
-labels_train = labels.iloc[val_n:]
+TRAIN_DIR = '../input/siim-covid19-detection/train'
+TEST_DIR = '../input/siim-covid19-detection/test'
+
+train = pd.read_csv('../input/siim-covid19-detection/train_image_level.csv')
+train_study = pd.read_csv('../input/siim-covid19-detection/train_study_level.csv')
+train_study['id'] = train_study['id'].apply(lambda i: i.split('_')[0])
+
+# handling studies with multiple images
+multi = train.StudyInstanceUID.value_counts()[train.StudyInstanceUID.value_counts()>1]
+to_remove = []
+for study in multi.index:
+    d = train[train['StudyInstanceUID']==study]
+    tup = (d['boxes'].isna().sum(), len(d), d['StudyInstanceUID'].iloc[0])
+    if tup[0]<tup[1]:
+        to_remove += d['boxes'][d['boxes'].isna()].index.tolist()
+train = train.drop(index=to_remove)
 
 
-class ChemicalDataset(Dataset):
-    def __init__(self, dir_path, labels_data, only_resize=False):
+class CovLungDataset(Dataset):
+    def __init__(self, dir_path, labels_data, new_size=(512,512)):
+        self.COUNTER = 0
         self.dir_path = dir_path
         self.labels_data = labels_data
-        self.only_resize = only_resize
+        self.new_size = new_size
 
     def __len__(self):
         return len(self.labels_data)
@@ -30,44 +41,26 @@ class ChemicalDataset(Dataset):
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-
-        img_id = self.labels_data.iloc[idx,0]
-        img_label = self.labels_data.iloc[idx,1]
-        img_dir = '/'.join(list(img_id[:3]))
-        image = cv2.imread(self.dir_path + img_dir + '/'+ img_id + '.png', 0)
-        image = ~image.astype('bool')
-        image = image.astype('uint8')
-        image = preprocessing.remove_noise(image, 2)
-        image = preprocessing.crop(image)
-        height, width = image.shape
-        if height > width:
-                image = np.rot90(image)
-        good_height, good_width = 300, 500
-        if height<=good_height and width<=good_width and not self.only_resize:
-            image_padded = np.zeros((good_height,good_width))
-            i = int((good_height-height)/2)
-            j = int((good_width-width)/2)
-            image_padded[i:i+height, j:j+width] = image
-        else:
-            ratio = width / height
-            good_ratio = good_width / good_height
-            if ratio < good_ratio:    # padding in width
-                new_width = int(np.round(height * good_ratio))
-                image_padded = np.zeros((height,new_width))
-                j = int((new_width-width)/2)
-                image_padded[:, j:j+width] = image
-            else:    # padding in height
-                new_height = int(np.round(width / good_ratio))
-                image_padded = np.zeros((new_height,width))
-                i = int((new_height-height)/2)
-                image_padded[i:i+height,:] = image
-            image_padded = cv2.resize(image_padded, (good_width,good_height), interpolation=cv2.INTER_NEAREST)
-        sample = {'image': image_padded, 'label': img_label}
+        
+        image_dir = self.labels_data.iloc[idx]['id']
+        img_label = self.labels_data.iloc[idx]['Negative for Pneumonia']
+        
+        path_to_img = os.path.join(self.dir_path, image_dir)
+        path_to_img = os.path.join(path_to_img, listdir(path_to_img)[0]) 
+        path_to_img = os.path.join(path_to_img, next(walk(path_to_img))[2][0])
+        data = pydicom.dcmread(path_to_img)
+        image = data.pixel_array
+        image = exposure.equalize_hist(image)
+        
+        good_height, good_width = self.new_size
+        image = cv2.resize(image, (good_width, good_height), interpolation=Image.LANCZOS)
+        sample = {'image': image.reshape(1, good_height, good_width), 'label': img_label}
         return sample
 
-transformed_dataset = ChemicalDataset(dir_path='/content/drive/MyDrive/ADP/',
-                                      labels_data=labels_train,
-                                      only_resize=False)
 
-dataloader = DataLoader(transformed_dataset, batch_size=4,
-                                                shuffle=True, num_workers=0)
+transformed_dataset = CovLungDataset(dir_path=TRAIN_DIR,
+                                     labels_data=train_study[['id', 'Negative for Pneumonia']],
+                                     new_size=(424,512))   # or 512x512 or 256x256
+
+dataloader = DataLoader(transformed_dataset, batch_size=32,
+                        shuffle=False, num_workers=0)
